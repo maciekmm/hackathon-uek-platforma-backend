7f4177d56744f89c3efbb331acd0b445cc92c8a8
Maciej Mionskowski <business@maciekmm.net>
2017-06-06 13:34:55 +0200

    Added test code for messenger

--- controllers/channels/messenger.go
+++ controllers/channels/messenger.go
@@ -21,6 +21,8 @@ func (m *Messenger) Register(router *mux.Router) {
 		AccessToken: os.Getenv("FB_ACCESS_TOKEN"),
 	}
 	m.messenger.Authentication = m.AuthenticationHandler
+	m.messenger.MessageReceived = m.ReceivedHandler
+
 	if os.Getenv("DEBUG") == "TRUE" {
 		m.messenger.Debug = messenger.DebugAll
 	}
@@ -28,6 +30,18 @@ func (m *Messenger) Register(router *mux.Router) {
 }
 
 func (m *Messenger) AuthenticationHandler(event messenger.Event, opts messenger.MessageOpts, msg *messenger.Optin) {
+	profile, err := m.messenger.GetProfile(opts.Sender.ID)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	_, err = m.messenger.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("Hello, %s %s", profile.FirstName, profile.LastName))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (m *Messenger) ReceivedHandler(event messenger.Event, opts messenger.MessageOpts, msg messenger.ReceivedMessage) {
 	profile, err := m.messenger.GetProfile(opts.Sender.ID)
 	if err != nil {
 		fmt.Println(err)
